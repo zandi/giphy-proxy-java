3dba976a3586cfac13ad9b0db74ae5311d2ed5ff
Michael Zandi <the.zandi@gmail.com>
2020-06-28 18:34:09 -0400

    Add barebones giphy test client using curl.
    It appears we can test the HTTP proxy tunneling with curl, which
    is very conveninent! Yes, there's an API key in here, but it's
    unimportant and disposable.

---
+++ test-giphy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# direct test (saniyt)
+curl "https://api.giphy.com/v1/gifs/search?q=test&api_key=fN39vpx7tywdcEf3OqnhTCzxMO5wNuo2&limit=2"
+
+echo
+echo 
+
+# note: curl gives us the following tunnel proxy command doing this:
+#  CONNECT api.giphy.com:443 HTTP/1.1
+#  Host: api.giphy.com:443
+#  User-Agent: curl/7.52.1
+#  Proxy-Connection: Keep-Alive
+# -x: proxy to use
+# -p: use tunneling
+# default port for proxy: 1080
+curl -x http://localhost -p "https://api.giphy.com/v1/gifs/search?q=test&api_key=fN39vpx7tywdcEf3OqnhTCzxMO5wNuo2&limit=2"
